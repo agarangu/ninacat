70b8e7246a8c1439ce221f335153c08ac586700a
Angel Aranguiz <angel.aranguiz@gmail.com>
2025-04-09 18:59:53 -0400

    Changed fallback locale

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -10,7 +10,7 @@ import { createI18n } from 'vue-i18n'
 
 const i18n = createI18n({
   locale: navigator.language.split('-')[0],
-  fallbackLocale: 'es',
+  fallbackLocale: 'en',
   messages: {
     es: {
       header: {
